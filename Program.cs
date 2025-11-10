d8767f40b7b59e744377a993c39f357dc554dd50
QuocThang <phanquocthang211005@gmail.com>
2025-11-10 22:02:19 +0700

    Clean up ignored files and add .gitignore

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using CodeForge_Winform.CodeForge_Winform.Presentation.Forms;
+
+namespace CodeForge_Winform
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
